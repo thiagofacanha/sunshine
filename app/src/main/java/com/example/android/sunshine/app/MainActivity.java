b0cbe29500692f5c23e58a3c2949fe7ae58e7643
Thiago Façanha <Thiago Faanha>
2017-01-16 23:25:12 -0200

    Using real data now with a call to API. Parsing JSON data

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,17 +1,9 @@
 package com.example.android.sunshine.app;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,12 +12,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+  
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,35 +43,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ArrayList<String> mFakeData = new ArrayList<>();
-            mFakeData.add("Today - Sunny -  88/63");
-            mFakeData.add("Tomorrow - Foggy -  70/46");
-            mFakeData.add("Weds - Cloudy -  72/63");
-            mFakeData.add("Thurs - Rainy -  64/51");
-            mFakeData.add("Fri - Foggy -  70/646");
-            mFakeData.add("Sat - Sunny -  76/68");
-
-            ArrayAdapter<String> mForecastArrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_text_view, mFakeData);
-
-            ListView mForecastListView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            mForecastListView.setAdapter(mForecastArrayAdapter);
-
-            return rootView;
-        }
-
-
-    }
 }
